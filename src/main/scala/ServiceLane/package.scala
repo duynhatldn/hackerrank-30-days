4185d50b95443932eee3c1feff30678b409f61ac
duynhatldn <nhatnhat393@gmail.com>
2018-12-15 23:58:29 +0700

    ServiceLane

---
+++ src/main/scala/ServiceLane/package.scala
@@ -0,0 +1,24 @@
+package object ServiceLane {
+  def calcMaxUnit(i:Int, j:Int, widthArray:Array[Int]) : Int = {
+    var position = 0
+    var maxUnit = 3
+    for(position <- i to j) {
+      if(widthArray(position) < maxUnit) {
+        maxUnit = widthArray(position)
+      }
+    }
+    return maxUnit
+  }
+
+
+  def main(args: Array[String]) {
+    val Array(n,t) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+    val widthArray = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+
+    var _ = 0
+    for(_ <- 0 until t) {
+      val Array(i,j) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+      println(calcMaxUnit(i,j,widthArray))
+    }
+  }
+}
