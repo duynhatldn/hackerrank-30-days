0a0edc5499ee3f4ef550a9ef784624c807157498
duynhatldn <nhatnhat393@gmail.com>
2018-12-20 23:13:16 +0700

    EqualizeTheArray

---
+++ src/main/scala/EqualizeTheArray/package.scala
@@ -0,0 +1,25 @@
+import java.io._
+package object EqualizeTheArray {
+  def equalizeArray(arr: Array[Int]): Int = {
+    val l = arr.toList
+    val distinctL = l.distinct
+    val max = distinctL.map(e => l.count(_ == e)).max
+    l.length - max
+
+  }
+
+  def main(args: Array[String]) {
+    val stdin = scala.io.StdIn
+
+    val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+    val n = stdin.readLine.trim.toInt
+
+    val arr = stdin.readLine.split(" ").map(_.trim.toInt)
+    val result = equalizeArray(arr)
+
+    printWriter.println(result)
+
+    printWriter.close()
+  }
+}
